41a308be64ee5e91cb5e3ba3f854ba62ca0f5446
xieyuhang <13451213869@163.com>
2023-07-21 09:56:56 +0800

    chore: init

---
+++ packages/components/Example/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>TODO: Example Component</div>
+</template>
+
+<style lang="css" scoped></style>
---
+++ packages/hooks/useExample/index.ts
@@ -0,0 +1,7 @@
+import { onMounted } from 'vue'
+
+export function useExample() {
+  onMounted(() => {
+    console.warn('TODO: Implement useExample hook')
+  })
+}
