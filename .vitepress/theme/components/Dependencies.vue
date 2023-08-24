92458da184281d0876e2519a36bc329eab525cd4
xieyuhang <13451213869@163.com>
2023-08-24 17:45:58 +0800

    docs: update index.md

--- .vitepress/theme/components/Dependencies.vue
+++ .vitepress/theme/components/Dependencies.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
-const props = defineProps<{
-  deps: string
-}>()
+const props = withDefaults(
+  defineProps<{
+    deps: string
+    showLabel?: boolean
+  }>(), {
+    showLabel: true,
+  },
+)
 
 const depsArr = computed(() => props.deps.split(',').map(dep => dep.trim()))
 </script>
 
 <template>
-  <p>依赖项：<a v-for="dep in depsArr" :key="dep" target="_bank" :href="`https://www.npmjs.com/package/${dep}`">{{ dep }}</a></p>
+  <span class="inline-block">
+    <span v-if="showLabel">依赖项：</span>
+    <a
+      v-for="dep in depsArr"
+      :key="dep"
+      class="mr-2"
+      target="_bank"
+      :href="`https://www.npmjs.com/package/${dep}`"
+    >
+      {{ dep }}
+    </a>
+  </span>
 </template>
