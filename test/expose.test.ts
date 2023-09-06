8c7b87f2f07e6e5681366a1c407a6a6bcf9a7bf2
xieyuhang <13451213869@163.com>
2023-09-06 16:18:40 +0800

    test: add test cases

--- test/expose.test.ts
+++ test/expose.test.ts
@@ -6,18 +6,19 @@ import AnimateDigits from 'ufuse/src/components/AnimateDigits/index.vue'
 import SplineView from 'ufuse/src/components/SplineView/index.vue'
 import EyeCryptButton from 'ufuse/src/components/EyeCryptButton/index.vue'
 
-import vPull from 'ufuse/src/directives/vPull'
-import vCompactFit from 'ufuse/src/directives/vCompactFit'
+import * as vPull from 'ufuse/src/directives/vPull'
+import * as vCompactFit from 'ufuse/src/directives/vCompactFit'
 
-import useAbortController from 'ufuse/src/hooks/useAbortController'
-import useContain from 'ufuse/src/hooks/useContain'
+import * as useAbortController from 'ufuse/src/hooks/useAbortController'
+import * as useContain from 'ufuse/src/hooks/useContain'
 
-import combine from 'ufuse/src/utils/combine'
-import toNumber from 'ufuse/src/utils/toNumber'
-import formatNumber from 'ufuse/src/utils/formatNumber'
-import ensureArray from 'ufuse/src/utils/ensureArray'
-import preloadAssets from 'ufuse/src/utils/preloadAssets'
-import createPromise from 'ufuse/src/utils/createPromise'
+import * as combine from 'ufuse/src/utils/combine'
+import * as toNumber from 'ufuse/src/utils/toNumber'
+import * as formatNumber from 'ufuse/src/utils/formatNumber'
+import * as ensureArray from 'ufuse/src/utils/ensureArray'
+import * as preloadAssets from 'ufuse/src/utils/preloadAssets'
+import * as createPromise from 'ufuse/src/utils/createPromise'
+import * as alignLottieAnimateElementsFrames from 'ufuse/src/utils/alignLottieAnimateElementsFrames'
 
 describe('ufuse', () => {
   it('expose', () => {
@@ -29,19 +30,31 @@ describe('ufuse', () => {
     expect(EyeCryptButton).toBeDefined()
 
     // directives
-    expect(vPull).toBeDefined()
-    expect(vCompactFit).toBeDefined()
+    expect(vPull.default).toBeDefined()
+    expect(vPull.vPull).toBeDefined()
+    expect(vCompactFit.default).toBeDefined()
+    expect(vCompactFit.vCompactFit).toBeDefined()
 
     // hooks
-    expect(useAbortController).toBeDefined()
-    expect(useContain).toBeDefined()
+    expect(useAbortController.default).toBeDefined()
+    expect(useAbortController.useAbortController).toBeDefined()
+    expect(useContain.default).toBeDefined()
+    expect(useContain.useContain).toBeDefined()
 
     // utils
-    expect(combine).toBeDefined()
-    expect(toNumber).toBeDefined()
-    expect(formatNumber).toBeDefined()
-    expect(ensureArray).toBeDefined()
-    expect(preloadAssets).toBeDefined()
-    expect(createPromise).toBeDefined()
+    expect(combine.default).toBeDefined()
+    expect(combine.combine).toBeDefined()
+    expect(toNumber.default).toBeDefined()
+    expect(toNumber.toNumber).toBeDefined()
+    expect(formatNumber.default).toBeDefined()
+    expect(formatNumber.formatNumber).toBeDefined()
+    expect(ensureArray.default).toBeDefined()
+    expect(ensureArray.ensureArray).toBeDefined()
+    expect(preloadAssets.default).toBeDefined()
+    expect(preloadAssets.preloadAssets).toBeDefined()
+    expect(createPromise.default).toBeDefined()
+    expect(createPromise.createPromise).toBeDefined()
+    expect(alignLottieAnimateElementsFrames.default).toBeDefined()
+    expect(alignLottieAnimateElementsFrames.alignLottieAnimateElementsFrames).toBeDefined()
   })
 })
